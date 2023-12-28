7635fccb3359a86ea1dfa439ea0284d0e3e9b544
praveen <mallulapraveen@gmail.com>
2023-12-29 00:23:57 +0530

    P3

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	euler "euler/goeuler"
+	"time"
 )
 
 func main() {
+	start_time := time.Now()
 	fmt.Println(euler.P1())
+	fmt.Println("Time taken: ", time.Since(start_time).Abs())
 }
